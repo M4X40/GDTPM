7e0a321f39a0fe828435d47f06940481b4e897fd
Max <maxst.marie11@gmail.com>
2023-08-09 21:41:20 -0700

    Add project files.

---
+++ GDPackLibrary/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System.Drawing;
+
+namespace GDPackLibrary
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		public void LoadingAnim()
+		{
+			label2.BringToFront();
+			label2.Visible = true;
+			LoadingTimer.Enabled = true;
+			LoadingTimer.Start();
+		}
+
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.LoadingTimer = new System.Windows.Forms.Timer(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// LoadingTimer
+			// 
+			this.LoadingTimer.Interval = 750;
+			this.LoadingTimer.Tick += new System.EventHandler(this.Timer_Tick);
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Pusab", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(395, 219);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(207, 51);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Loading";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Image = global::GDPackLibrary.Properties.Resources.LoadingGradient;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(942, 493);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(27, 106);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(886, 365);
+			this.panel1.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.BackgroundImage = global::GDPackLibrary.Properties.Resources.LoadingGradient;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(942, 493);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label2);
+			this.Font = new System.Drawing.Font("JetBrainsMono Nerd Font Mono", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "Form1";
+			this.Text = "GD Pack Library | Loading";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer LoadingTimer;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ GDPackLibrary/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using GDPackLibrary.Properties;
+using System.Text.RegularExpressions;
+
+namespace GDPackLibrary
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			LoadingAnim();
+		}
+
+		private int amount = 0;
+		
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			if (amount == 0 || amount == 4)
+			{
+				label2.Text = "Loading.";
+			}
+			else if (amount == 1 || amount == 5)
+			{
+				label2.Text = "Loading..";
+			}
+			else if (amount == 2 || amount == 6)
+			{
+				label2.Text = "Loading...";
+			}
+			else if (amount == 3 || amount == 7)
+			{
+				label2.Text = "Loading";
+			}
+			else if (amount == 8)
+			{
+				LoadingTimer.Interval = 50;
+			}
+			else if (amount == 9)
+			{
+				label2.Text = "oadin";
+			}
+			else if (amount == 10)
+			{
+				label2.Text = "adi";
+			}
+			else if (amount == 11)
+			{
+				label2.Text = "d";
+			}
+			else if (amount == 12)
+			{
+				label2.Text = "";
+				LoadingTimer.Interval = 100;
+			}
+			else if (amount == 13)
+			{
+				label2.Text = "d";
+				LoadingTimer.Interval = 100;
+			}
+			else if (amount == 14)
+			{
+				label2.Text = "do";
+			}
+			else if (amount == 15)
+			{
+				label2.Text = "don";
+			}
+			else if (amount == 16)
+			{
+				label2.Text = "done";
+			}
+			else if (amount == 17)
+			{
+				label2.Text = "done!";
+				LoadingTimer.Interval = 750;
+			}
+			else if (amount == 18)
+			{
+				LoadingTimer.Interval = 50;
+			}
+			else if (amount == 19)
+			{
+				label2.Text = "one";
+			}
+			else if (amount == 20)
+			{
+				label2.Text = "n";
+			}
+			else if (amount == 21)
+			{
+				label2.Text = "";
+				label2.Visible = false;
+				LoadingTimer.Stop();
+			}
+			amount += 1;
+			if (amount <= 20)
+			{
+				this.Text = "GD Pack Library | " + (label2.Text);
+			} else
+			{
+				this.Text = "GD Pack Library";
+			}
+			label2.Location = new Point((this.Width / 2) - (label2.Width / 2), (this.Height / 2) - (label2.Height / 2) - 27);
+
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+		private void Form1_ResizeEnd(object sender, EventArgs e)
+		{
+			label2.Location = new Point((this.Width / 2) - (label2.Width / 2), (this.Height / 2) - (label2.Height / 2) - 27);
+		}
+	}
+}
