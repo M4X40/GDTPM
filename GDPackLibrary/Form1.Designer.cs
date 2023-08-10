7e248f8092a025357ec84c3f585321b9f7bacb85
ZenDev <76600019+ZenCreates@users.noreply.github.com>
2023-08-09 21:46:20 -0700

    testing2

--- GDPackLibrary/Form1.Designer.cs
+++ GDPackLibrary/Form1.Designer.cs
@@ -83,7 +83,7 @@ namespace GDPackLibrary
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(23, 35);
+            this.panel1.Location = new System.Drawing.Point(24, 105);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(886, 365);
             this.panel1.TabIndex = 5;
